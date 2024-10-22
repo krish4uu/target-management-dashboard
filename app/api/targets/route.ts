926772898ae3dfd18d5e351e9350881b9d345c6c
krish4uu <prajapatikashyap97@gmail.com>
2024-10-22 02:42:03 -0400

    API handler for CRUD operations

--- app/api/targets/route.ts
+++ app/api/targets/route.ts
@@ -1,6 +1,29 @@
-import { NextResponse } from 'next/server'
-import targetsData from '../../../data/targets.json'
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import targetsData from "../../../data/targets.json";
 
 export async function GET() {
-  return NextResponse.json(targetsData)
+  return NextResponse.json(targetsData);
+}
+
+export async function PUT(req: Request) {
+  const { id, pipelineStatus } = await req.json();
+  const targetIndex = targetsData.findIndex((target) => target.id === id);
+
+  if (targetIndex === -1) {
+    return NextResponse.json({ error: "Target not found" }, { status: 404 });
+  }
+
+  // Update the pipeline status and date
+  targetsData[targetIndex].pipelineStatus = pipelineStatus;
+  targetsData[targetIndex].lastUpdated = new Date().toISOString()
+
+  // updating the JSON file
+  fs.writeFileSync(
+    path.join(process.cwd(), "data/targets.json"),
+    JSON.stringify(targetsData, null, 2)
+  );
+
+  return NextResponse.json(targetsData[targetIndex]);
 }
