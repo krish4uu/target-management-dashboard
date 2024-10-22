2c72c0115de2b6c2ace18eb8dbea5444f9e2f5cc
krish4uu <prajapatikashyap97@gmail.com>
2024-10-22 02:41:17 -0400

    Types for the components

--- lib/types.ts
+++ lib/types.ts
@@ -6,3 +6,17 @@ export interface Target {
     markets: string[];
     lastUpdated: string;
   }
+
+  export interface TargetTableProps {
+    filter: string;
+  }
+
+  export interface BarChartData {
+    status: string;
+    count: number;
+  }
+  
+  export interface BarChartProps {
+    filter: string;
+  }
+  
