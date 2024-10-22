fcf85aa9e9461bcad83b4bb8ef25422b04fb79d4
krish4uu <prajapatikashyap97@gmail.com>
2024-10-22 02:41:01 -0400

    Homepage with Bar and dashboard link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import BarChart from "@/components/BarChart";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Target Management Dashboard</h1>
-      <Link href="/dashboard" className="text-blue-500 hover:underline">
-        Go to Dashboard
-      </Link>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <Navbar />
+      <div className="flex flex-col pt-8 justify-center items-center">
+        <h1 className="text-3xl md:text-4xl font-bold mb-8">
+          Target Management
+        </h1>
+        <Link
+          href="/dashboard"
+          className="text-2xl text-blue-500 hover:underline"
+        >
+          Go to Dashboard
+        </Link>
+      </div>
+      <BarChart filter="" />
+      <Footer />
     </main>
   );
 }
